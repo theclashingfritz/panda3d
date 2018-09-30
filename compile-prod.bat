b2108ab0ee5c5255be44834253e2c64f8e4ea2ea
unknown <theclashingfritz@gmail.com>
2018-09-30 10:50:24 -0400

    Give artifacts a custom path

--- compile-prod.bat
+++ compile-prod.bat
@@ -1,3 +1,3 @@
 @echo off
-thirdparty\win-python\python makepanda/makepanda.py --everything --verbose --optimize=4 --no-eigen %*
+thirdparty\win-python\python makepanda/makepanda.py --everything --windows-sdk=10 --verbose --optimize=4 --no-eigen %*
 pause
